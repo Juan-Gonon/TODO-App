cf1ee4542ee5f950e5fadf84b1e99cd0702ff85f
Juan Gonon <juanfegonon27@gmail.com>
2024-02-28 15:42:50 -0600

    add cards

--- script.js
+++ script.js
@@ -6,7 +6,30 @@ const createTask = (e)=>{
     e.preventDefault();
 
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value)
+    const task = document.querySelector("[data-tag]");
+
+
+
+    const value = input.value;
+
+
+    const content = `
+    <li class="card">
+            <div>
+            <i class="far fa-check-square icon"></i>
+            <span class="task">${value}</span>
+            </div>
+            <i class="fas fa-trash-alt trashIcon icon"></i>
+    </li>
+    `
+
+    task.innerHTML += content
+
+    console.log(value)
+    console.log(task)
+    input.value = "";
+
+
 
 }
 
